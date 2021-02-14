c2c5c23464da1a9f131d6a5b538d7a459c3b4430
Timur Khrustalyov <tilvlur@gmail.com>
2021-02-14 18:14:12 +0300

    Ajax #2. JavaScript, success, callback

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 'use strict';
 
-const resultBlock = document.querySelector('#result')
+const resultBlock = document.querySelector('#result');
 const clickMeBtn = document.querySelector('#click-me');
 
 clickMeBtn.addEventListener('click', makeRequest);
 
 let a = 5;
-$.ajax('https://repetitora.net/api/JS/Images')
+$.ajax('https://repetitora.net/api/JS/Images', {
+  success(data) {
+    data.forEach(el => {
+      let img = document.createElement('img');
+      img.src = el.original;
+      img.alt = 'Image from AJAX-request';
+      document.body.append(img);
+    });
+
+    for (let i = 0; i < data.length; i++) {
+      let img = document.createElement('img');
+      img.src = data[i].original;
+      img.alt = 'Image from AJAX-request';
+      document.body.append(img);
+    }
+  },
+});
 a = 8;
 console.log(a);
 
