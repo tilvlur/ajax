70b4eda2ac42fbc7d13d38110d02c23b022cfc39
Timur Khrustalyov <tilvlur@gmail.com>
2021-02-14 22:05:16 +0300

    Ajax #3. GET, query-params, GET-параметры, query-string

--- index.js
+++ index.js
@@ -2,30 +2,28 @@
 
 const resultBlock = document.querySelector('#result');
 const clickMeBtn = document.querySelector('#click-me');
+const pageNumber = document.querySelector('#inputNumber');
 
-clickMeBtn.addEventListener('click', makeRequest);
-
-let a = 5;
-$.ajax('https://repetitora.net/api/JS/Images', {
-  success(data) {
-    data.forEach(el => {
-      let img = document.createElement('img');
-      img.src = el.original;
-      img.alt = 'Image from AJAX-request';
-      document.body.append(img);
-    });
-
-    for (let i = 0; i < data.length; i++) {
-      let img = document.createElement('img');
-      img.src = data[i].original;
-      img.alt = 'Image from AJAX-request';
-      document.body.append(img);
-    }
-  },
+clickMeBtn.addEventListener('click', () => {
+  makeRequest(pageNumber.value);
 });
-a = 8;
-console.log(a);
 
-function makeRequest() {
-  console.log('Request has making');
+function makeRequest(pageNumber) {
+  $.ajax(`https://repetitora.net/api/JS/Images?page=${pageNumber}&count=1`, {
+    success(data) {
+      data.forEach(el => {
+        let img = document.createElement('img');
+        img.src = el.original;
+        img.alt = 'Image from AJAX-request';
+        resultBlock.append(img);
+      });
+
+      /*for (let i = 0; i < data.length; i++) {
+        let img = document.createElement('img');
+        img.src = data[i].original;
+        img.alt = 'Image from AJAX-request';
+        document.body.append(img);
+      }*/
+    },
+  });
 }
