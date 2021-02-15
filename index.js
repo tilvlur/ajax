fc88aaa6d7aff5f03f54be4cf3b9925a4101d7bf
Timur Khrustalyov <tilvlur@gmail.com>
2021-02-16 00:03:27 +0300

    promise

--- index.js
+++ index.js
@@ -8,6 +8,35 @@ clickMeBtn.addEventListener('click', () => {
   getImages(pageNumber.value, renderImages);
 });
 
+doItAfter(2)
+    .then((daneMessage) => setTimeout(() => console.log(`Hey-Hey ${daneMessage}`), 10))
+    .then(() => setTimeout(() => console.log('Hello!'), 4000));
+
+hardLoadProcess(1)
+    .then((dane) => console.log(dane));
+
+let promise = new Promise((resolve) => {
+  setTimeout(() => resolve('done-done'), 1000);
+});
+
+promise.then((result) => console.log(result));
+
+//FUNCTIONS
+
+function doItAfter(seconds) {
+  return new Promise((resolve) => setTimeout(() => resolve('done'), seconds * 1000));
+}
+
+function hardLoadProcess(loadLevel) {
+  return new Promise((resolve) => {
+    for (let i = 0; i < loadLevel * 3; i++) {
+      console.log(Math.random() * 1000);
+    }
+    resolve('well done');
+  });
+
+}
+
 function renderImages(data) {
   data.forEach(el => {
     let img = document.createElement('img');
